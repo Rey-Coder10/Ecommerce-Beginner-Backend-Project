15eb66e2269840c3fabf862cec739f36b9016e92
Rahul <yadavrahul1042000@gmail.com>
2026-07-12 17:30:47 +0530

    FrontEnd and Backend pushed

---
+++ Ecommerce Project/Backend/src/main/java/com/project/snippet/ApplicationPortion.java
@@ -0,0 +1,13 @@
+package com.project.snippet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationPortion {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationPortion.class, args);
+    }
+
+}
